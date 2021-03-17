3c94b678ea50f48ecd64fbc451523d37463b37c1
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-17 18:59:08 -0400

    added some seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+group1 = Group.find_or_create_by(name: 'Calculus 1')
+group2 = Group.find_or_create_by(name: 'Physics 1')
