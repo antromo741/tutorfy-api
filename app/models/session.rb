bd988f2bebee2ffc9aeecb30921680773f252e38
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-18 14:28:53 -0400

    added a description to the tutoring sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,5 @@
 class Session < ApplicationRecord
+  has_one_attached :poster
 belongs_to :user
   belongs_to :group
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_155927) do
+ActiveRecord::Schema.define(version: 2021_03_18_182524) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_155927) do
     t.integer "users_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["group_id"], name: "index_sessions_on_group_id"
     t.index ["users_id"], name: "index_sessions_on_users_id"
   end
