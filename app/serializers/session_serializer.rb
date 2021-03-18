ee8e4e6d47571c61620a52d1efe118f170cb4f83
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-18 17:19:01 -0400

    added poster url to stay with the session model, updated the session serializer

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,4 @@
 class SessionSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :description, :start_time, :end_time, :location, :poster
+  attributes :id, :name, :description, :start_time, :end_time, :location, :poster_url
 end
