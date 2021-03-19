e3e8e7b7db3bb771a3dff287c7e11ea42925b861
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-19 15:23:33 -0400

    fixed a params issue in session controller and added active storage table

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,9 +1,9 @@
 class Session < ApplicationRecord
   has_one_attached :poster
   belongs_to :user
-    belongs_to :group
+  belongs_to :group
 
   def poster_url
-  Rails.application.routes.url_helpers.url_for(poster) if poster.attached?
-end
+    Rails.application.routes.url_helpers.url_for(poster) if poster.attached?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2021_03_18_193604) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -66,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_03_18_193604) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "sessions", "groups"
   add_foreign_key "sessions", "users"
 end
