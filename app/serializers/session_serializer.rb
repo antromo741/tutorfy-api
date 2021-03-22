30a4ae139e9eba5d0dfe019dfa0911bd051f372c
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-22 19:25:13 -0400

    did not have group_id in my params so it wasnt getting saved

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,4 @@
 class SessionSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :description, :start_time, :end_time, :location, :poster_url
+  attributes :id, :name, :description, :start_time, :end_time, :location, :poster_url, :group_id
 end
