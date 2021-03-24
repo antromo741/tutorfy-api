36815bdc732c33b81af158644c4e6434e399f3c6
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-03-24 16:29:20 -0400

    added validations into session model

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,6 +3,17 @@ class Session < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
+  validates :name,:location, :description, :start_time, :end_time, presence: true
+  validates :name, uniqueness: {scope: [:start_time, :location, :group_id], message: "Whoops! Did you already create this session?"}, if: Proc.new { |session| session.name.present? }
+  validate :validate_start_time, if: Proc.new { |session| session.start_time.present? && session.end_time.present? }
+  
+  def validate_start_time
+    if start_time >= end_time
+      errors.add(:start_time, "must be before the end time")
+    end
+  end
+
+
   def poster_url
     Rails.application.routes.url_helpers.url_for(poster) if poster.attached?
   end
